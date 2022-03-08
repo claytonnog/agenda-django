cbdedb826e40fb94eaf1d8699c7754719f09731a
Neves-Clayton <Neves_Clayton@ne.bah.com>
2022-03-08 15:45:45 -0300

    added login and logout system

--- agenda/settings.py
+++ agenda/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contatos.apps.ContatosConfig'
+    'contatos.apps.ContatosConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
